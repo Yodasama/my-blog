0e1912df9ff251be253b0abcda9be8b30d3cb04c
Yodasama <530049255@qq.com>
2026-05-15 16:03:12 +0800

    Add homepage category filtering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
@@ -36,5 +34,4 @@ export default defineConfig({
       },
 	],
 
-  adapter: cloudflare(),
 });
